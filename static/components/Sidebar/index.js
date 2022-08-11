773ac56d8b2995c306318c0ca1924a4a8a32dc8f
PierceCCH <pierce.chongch@gmail.com>
2022-08-11 15:08:57 +0800

    removed port number

--- static/components/Sidebar/index.js
+++ static/components/Sidebar/index.js
@@ -29,7 +29,7 @@ const Sidebar = ({isOpen, toggle}) => {
                     <NavLinks to="sponsorships" smooth={true} duration={400} spy={true} exact='true' offset={-60}  onClick={toggle}>Partnerships</NavLinks>
                 </NavItem>
                 <NavItem>
-                    <SotongButton href='https://discord.gg/rNzYBUFx6a' target="_blank">Sotong Guide</SotongButton>
+                    <SotongButton href='https://discord.gg/rNzYBUFx6a' target="_blank">Join Us!</SotongButton>
                 </NavItem>
                 <NavItem>
                     <SotongButton href="https://medium.com/@ssa.uclabruins/ucla-ssa-sotong-guide-6e68161a2506" target="_blank">Sotong Guide</SotongButton>
