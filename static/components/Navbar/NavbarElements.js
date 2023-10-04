17faac183a5b46aa1dedf99356b261d2cb96bf0c
Pierce Chong <48197758+PierceCCH@users.noreply.github.com>
2023-10-03 17:55:45 -0700

    added committee members and rat

--- static/components/Navbar/NavbarElements.js
+++ static/components/Navbar/NavbarElements.js
@@ -30,7 +30,7 @@ export const NavbarContainer = styled.div`
     justify-content: space-between;
     height: 80px;
     z-index: 1;
-    width: 100%;
+    width: 70%;
     padding: 0 24px:
     max-width: 1100px;
 `;
