a9543aefbe935e154e4d23abf05e367f9ad2d2a9
Alex Su <suziyue@gmail.com>
2025-02-04 17:29:51 -0800

    Changed to jsx

--- src/components/Partnerships/PartnershipsElements.js
+++ src/components/Partnerships/PartnershipsElements.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Colors from "../styles.js";
+import Colors from "../styles";
 
 export const PartnershipsContainer = styled.div`
   background: ${Colors.offWhite};
