5f0677d04a881c55fba8adda69139ab1de171b07
Alex Su <suziyue@gmail.com>
2025-02-04 18:11:46 -0800

    added babel-plugin-styled-components

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig, transformWithEsbuild } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: ['babel-plugin-styled-components']
+      }
+    }),
   ],
 })
