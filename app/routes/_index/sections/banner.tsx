e31897e9b0a4cc35363b90ff79bc87e2740c335b
Alex Su <suziyue@gmail.com>
2025-04-13 11:10:17 -0700

    Refactored marquee for extensibility

--- app/routes/_index/sections/banner.tsx
+++ app/routes/_index/sections/banner.tsx
@@ -6,13 +6,15 @@ export default function Banner() {
         <a
             href="https://t.me/+rtKVQe-ui69iYmY1"
             target="_blank"
-            className="z-2 flex h-10 w-full flex-col items-center overflow-hidden bg-red whitespace-nowrap"
+            className="z-2 flex h-10 w-full flex-row items-center overflow-hidden bg-red tracking-wider whitespace-nowrap text-offwhite"
         >
-            <div className="my-auto flex animate-scroll-x space-x-100 tracking-wider text-offwhite">
-                <span className="">{text}</span>
-                <span className="">{text}</span>
-                <span className="">{text}</span>
-                <span className="">{text}</span>
+            <div className="my-auto flex animate-[marquee_25s_infinite_linear]">
+                <span className="px-40">{text}</span>
+                <span className="px-40">{text}</span>
+            </div>
+            <div className="my-auto flex animate-[marquee_25s_infinite_linear]">
+                <span className="px-40">{text}</span>
+                <span className="px-40">{text}</span>
             </div>
         </a>
     );
