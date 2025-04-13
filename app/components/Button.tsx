0846c357779841a35e71b68bb4681f7b99bdba45
Alex Su <suziyue@gmail.com>
2025-04-13 11:48:12 -0700

    Added clsx

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 export default function Button(props: {
     primary?: boolean;
     size?: number;
@@ -6,36 +8,32 @@ export default function Button(props: {
     [key: string]: unknown;
 }) {
     const { primary, size, fontBig, className, ...restprops } = props;
-    let bg, bgHover, text, textHover;
-    if (primary) {
-        bg = "bg-offwhite";
-        bgHover = "hover:bg-red";
-        text = "text-black";
-        textHover = "hover:text-offwhite";
-    } else {
-        bg = "bg-red";
-        bgHover = "hover:bg-gold";
-        text = "text-offwhite";
-        textHover = "hover:text-black";
-    }
-
-    let padding;
-    switch (size ?? 1) {
-        case 0:
-            padding = "px-4 py-2.5";
-            break;
-        case 1:
-            padding = "px-7.5 py-3";
-            break;
-        case 2:
-            padding = "px-12 py-3.5";
-            break;
-    }
-    const fontsize = fontBig ? "text-[20px]" : "text-[16px]";
 
     return (
         <div
-            className={`flex w-max cursor-pointer items-center justify-center rounded-[50px] border-none whitespace-nowrap outline-none ${padding} ${fontsize} ${bg} ${text} transition duration-200 ease-in-out ${bgHover} ${textHover} ${className ?? ""}`}
+            className={clsx([
+                "flex w-max cursor-pointer items-center justify-center rounded-[50px] border-none whitespace-nowrap transition duration-200 ease-in-out outline-none",
+                className,
+                size === 0
+                    ? "px-4 py-2.5"
+                    : size === 2
+                      ? "px-12 py-3.5"
+                      : "px-7.5 py-3",
+                primary
+                    ? [
+                          "bg-offwhite",
+                          "hover:bg-red",
+                          "text-black",
+                          "hover:text-offwhite",
+                      ]
+                    : [
+                          "bg-red",
+                          "hover:bg-gold",
+                          "text-offwhite",
+                          "hover:text-black",
+                      ],
+                fontBig ? "text-[20px]" : "text-[16px]",
+            ])}
             {...restprops}
         />
     );
